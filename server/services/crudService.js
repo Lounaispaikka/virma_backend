45a434ff84daff2812ef47e4db322123a8bad3eb
Koivunen <github@koivunen.work>
2020-03-04 15:44:05 +0200

    Return 500 on error, also log error to console

--- server/services/crudService.js
+++ server/services/crudService.js
@@ -1,7 +1,7 @@
 const db = require('../../db');
 const Model = require('../../db');
 
-function createLog(operation, model, name = undefined, user = undefined) {
+function createLog(operation, model, name = undefined, user = undefined, message = undefined) {
 
   const params = {
     operation: operation,
@@ -10,7 +10,7 @@ function createLog(operation, model, name = undefined, user = undefined) {
     timestamp: new Date,
     executor: user ? user : undefined
   }
-
+  console.error("ERROR",operation,name,model.getTableName(),message);
   Model.logs.create(params);
 }
 
@@ -19,8 +19,8 @@ exports.createFeature = function(res, model, params, user) {
     createLog('CREATE', model, params.name_fi, user);
     res.json({ 'message': response });
   }).catch(error => {
-    createLog('CREATE_ERROR', model);
-    res.json({ 'message': error });
+    createLog('CREATE_ERROR', model, undefined, undefined, error);
+    res.status(500).json({ 'message': error });
   });
 }
 
@@ -29,8 +29,8 @@ exports.updateFeature = function(res, model, id, params, user) {
     createLog('UPDATE', model, params.name_fi, user);
     res.json({ 'message': response });
   }).catch(error => {
-    createLog('UPDATE_ERROR', model);
-    res.json({ 'message': error });
+    createLog('UPDATE_ERROR', model, undefined, undefined, error);
+    res.status(500).json({ 'message': error });
   });
 }
 
@@ -39,7 +39,7 @@ exports.deleteFeature = function(res, model, id, name, user) {
     createLog('DELETE', model, name, user);
     res.json({ 'message': response });
   }).catch(error => {
-    createLog('DELETE_ERROR', model);
-    res.json({ 'message': error });
+    createLog('DELETE_ERROR', model, undefined, undefined, error);
+    res.status(500).json({ 'message': error });
   });
 }
